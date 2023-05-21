1646505881b54379238c2a89d55fca1fd239f322
Baktybek uulu Nadyrbek <b.taku-93@mail.ru>
2023-05-21 15:31:12 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <img src="logo192.png"/>
+      </div>
+      <nav className="nav-menu">
+        <ul>
+          <li><a>Baktybek</a></li>
+          <li><a>Jamiyla</a></li>
+          <li><a>Ayperi</a></li>
+          <li><a>Nadyrbek</a></li>
+          <li><a>Aydana</a></li>
+          <li><a>Kurmangazy</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
