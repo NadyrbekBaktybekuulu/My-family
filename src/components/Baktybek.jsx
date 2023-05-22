98935d9edc58b6930cb72e6a559badf28f8550b4
Baktybek uulu Nadyrbek <b.taku-93@mail.ru>
2023-05-22 20:32:55 +0300

    added new files

---
+++ src/components/Baktybek.jsx
@@ -0,0 +1,10 @@
+
+const Baktybek = () => {
+   return (
+      <div className="user-card">
+         <h1 className="user-card-title">
+            Duyshenaliev Baktybek Joldoshbekovich
+         </h1>
+      </div>
+   )
+};
